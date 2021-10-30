9dd156420e33562595abb87551ee8dc3f875ca01
fedfon <fed.fonseca@gmail.com>
2021-10-29 22:08:16 -0400

    Changes ports of frontend and proxy, Changed the way the data is recieved from the res of the server.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 //express server
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 
 mongoose
   .connect(process.env.DB, {
